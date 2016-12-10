5e0cc55d9904f77779702ce9768f755405db6732
Cameron Tacklind <cameron@tacklind.com>
2016-12-09 23:49:58 -0800

    User server-starter with express correctly and set socket mode

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
 var serverStarter = require('server-starter');
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
 
 var proxyCamUrl = 'http://raspberryprint/webcam/?action=stream';
 
 var proxy = new MjpegProxy(proxyCamUrl);
 
 app.get('/live', proxy.proxyRequest);
-app.get('/snap', proxy.snapshot);
+app.get('/snap', proxy.snapshot || ((req, res) => {
+ res.send('');
+}));
 
 app.get('/numberClients', (req, res) => {
   res.send(proxy.audienceResponses.length);
@@ -16,6 +19,9 @@ app.get('/numberClients', (req, res) => {
 
 var config = {
  listen: 'printcam.socket',
+ socketMode: 0707,
 };
 
-serverStarter(app, config);
+serverStarter(server, config, addr => {
+ console.log(addr);
+});
